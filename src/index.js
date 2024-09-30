b40a340895854b0da5681330550057b758e2e886
Heloise Santos <heloise.hssantos@gmail.com>
2024-09-29 21:06:50 -0300

    Criação do projeto, instalação do express e mongoose, integração com mongodb e criação das operações crud

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+const Post = mongoose.model('Post', { 
+    title: String,
+    author: String,
+    description: String,
+    createDate: String,
+    updateDate: String
+});
+
+app.get('/', async (req, res) => {
+    const posts = await Post.find();
+    return res.send(posts);
+});
+
+app.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        createDate: req.body.createDate,
+        updateDate: req.body.updateDate
+    });
+
+    await post.save();
+    return res.send(post);
+});
+
+app.put('/:id', async (req, res) => {
+    const post = await Post.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        createDate: req.body.createDate,
+        updateDate: req.body.updateDate
+    }, {
+        new: true
+    });
+    return res.send(post);
+});
+
+app.delete('/:id', async (req, res) => {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    return res.send(post);
+});
+
+app.listen(port, () => {
+    mongoose.connect('mongodb+srv://heloisehssantos:ICbxVAGGRlZSLHgP@fiap-blog-backend.rl2oe.mongodb.net/?retryWrites=true&w=majority&appName=fiap-blog-backend');
+
+    console.log(`App running on port ${port}`);
+});
