51c080f98564d1da6d979791dbd2bfd1c8bc99b4
Heloise Santos <heloise.hssantos@gmail.com>
2024-10-07 21:09:40 -0300

    Criação de arquivo de rotas para isolar métodos de comunicação com o banco de dados

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const Post = require('../models/Post');
-
 app.use(
     express.urlencoded({
         extended: true,
@@ -13,48 +11,11 @@ app.use(
 
 app.use(express.json());
 
+const personRoutes = require('../routes/postRoutes');
+
+app.use('/posts', personRoutes);
+
 const port = 3000;
-
-app.get('/posts', async (req, res) => {
-    const posts = await Post.find();
-    return res.send(posts);
-});
-
-app.get('/posts/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    return res.send(post);
-});
-
-app.post('/posts', async (req, res) => {
-    const post = new Post({
-        title: req.body.title,
-        author: req.body.author,
-        description: req.body.description,
-        createDate: req.body.createDate,
-        updateDate: req.body.updateDate
-    });
-
-    await post.save();
-    return res.send(post);
-});
-
-app.put('/posts/:id', async (req, res) => {
-    const post = await Post.findByIdAndUpdate(req.params.id, {
-        title: req.body.title,
-        author: req.body.author,
-        description: req.body.description,
-        createDate: req.body.createDate,
-        updateDate: req.body.updateDate
-    }, {
-        new: true
-    });
-    return res.send(post);
-});
-
-app.delete('/posts/:id', async (req, res) => {
-    const post = await Post.findByIdAndDelete(req.params.id);
-    return res.send(post);
-});
 
 mongoose.connect(
     'mongodb+srv://heloisehssantos:ICbxVAGGRlZSLHgP@fiap-blog-backend.rl2oe.mongodb.net/?retryWrites=true&w=majority&appName=fiap-blog-backend'
