5be1b5887b32a448a55ba39ac3d8792b5c0ba767
jmozkji <jonas.andrade7@gmail.com>
2024-10-09 21:37:43 -0300

    Implement code status validation.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,45 +1,90 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Post = require('../models/Post');
+const Post = require("../models/Post");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
     const posts = await Post.find();
-    return res.send(posts);
+    return res.status(201).json(posts);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
+  try {
     const post = await Post.findById(req.params.id);
-    return res.send(post);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "There is no post with the requested id." });
+    }
+
+    return res.status(200).json(post);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  try {
     const post = new Post({
-        title: req.body.title,
-        author: req.body.author,
-        description: req.body.description,
-        createDate: req.body.createDate,
-        updateDate: req.body.updateDate
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+      createDate: req.body.createDate,
+      updateDate: req.body.updateDate,
     });
+
     await post.save();
-    return res.send(post);
+
+    return res.status(201).json(post);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
 });
 
-router.put('/:id', async (req, res) => {
-    const post = await Post.findByIdAndUpdate(req.params.id, {
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      {
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
         createDate: req.body.createDate,
-        updateDate: req.body.updateDate
-    }, {
-        new: true
-    });
-    return res.send(post);
+        updateDate: req.body.updateDate,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "There is no post with the requested id." });
+    }
+
+    return res.status(200).json(post);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', async (req, res) => {
-    const post = await Post.findByIdAndDelete(req.params.id);
-    return res.send(post);
+router.delete("/:id", async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.id);
+
+    return res.status(204).json();
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 module.exports = router;
