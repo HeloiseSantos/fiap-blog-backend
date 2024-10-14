5ec868fc6464e04108cf71aacdb764ea57397331
jmozkji <jonas.andrade7@gmail.com>
2024-10-14 09:14:10 -0300

    Update port variable.

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://heloisehssantos:ICbxVAGGRlZSLHgP@fiap-blog-backend.rl2oe.mongodb.net/?retryWrites=true&w=majority&appName=fiap-blog-backend"
+  )
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => console.log(err));
