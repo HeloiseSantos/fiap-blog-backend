c2fce7203df147725a2258e791317c6340f31f17
jmozkji <jonas.andrade7@gmail.com>
2024-10-14 19:44:20 -0300

    Create get post by id unit tests.

--- tests/postController.test.js
+++ tests/postController.test.js
@@ -49,3 +49,53 @@ describe("Post Controller", () => {
     });
   });
 });
+
+describe("getPostById", () => {
+  it("deve retornar um post por ID", async () => {
+    const req = { params: { id: "123" } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // Simula o Post.findById() retornando um post
+    Post.findById.mockResolvedValue({ title: "Post 1" });
+
+    await getPostById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ title: "Post 1" });
+  });
+
+  it("should return 404 if post is not found", async () => {
+    const req = { params: { id: "123" } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    Post.findById.mockResolvedValue(null);
+
+    await getPostById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "There is no post with the requested id.",
+    });
+  });
+
+  it("should return 500 if an error occurs", async () => {
+    const req = { params: { id: "123" } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    Post.findById.mockRejectedValue(new Error("Error fetching post"));
+
+    await getPostById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(expect.any(Error));
+  });
+});
