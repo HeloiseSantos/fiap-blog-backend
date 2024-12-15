9ec09ed37967a283b3a46eb86a419bef5cbb6d42
Heloise Santos <heloise.hssantos@gmail.com>
2024-12-14 21:44:36 -0300

    install middleware CORS

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 mongoose
   .connect(
-    "mongodb+srv://heloisehssantos:ICbxVAGGRlZSLHgP@fiap-blog-backend.rl2oe.mongodb.net/?retryWrites=true&w=majority&appName=fiap-blog-backend"
+    "mongodb+srv://heloisehssantos:Zu9uLlH61sp05t4Q@fiap-blog-backend.rl2oe.mongodb.net/?retryWrites=true&w=majority&appName=fiap-blog-backend"
   )
   .then(() => {
     app.listen(port, () => {
